57d1fd21164c06a0c48962e11acd6b5ee6043594
Nabil Ouachi <nabil.ouachi@gmail.com>
2018-12-29 15:44:24 +0100

    initial commit

---
+++ src/test/kotlin/com/n26/transactionstatistics/transaction/domain/TransactionTests.kt
@@ -0,0 +1,12 @@
+package com.n26.transactionstatistics.transaction.domain
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class TransactionTests {
+
+    @Test
+    fun name() {
+        assertTrue {  1 == 1 }
+    }
+}
